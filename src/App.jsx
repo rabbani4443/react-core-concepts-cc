f3aa5779ef6987082550569e3d6b910b703de83b
rabbani4443 <rabbani07r@gmail.com>
2023-09-17 10:17:42 +0600

    README.md file has been deleted

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,16 @@ import Card2 from './components/Card2/Card2'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  let newTitle = 'My New Title'
+  let title2= "Your New Title"
+  let lastTitle = "Title The End"
+   const myObj ={
+    views:'100k',
+    views2:'99k',
+    views3:'5.5k',
+   }
+
 
   return (
     <>
@@ -19,9 +28,9 @@ function App() {
 
      <h1 className="text-5xl font-bold text-center py-8 mt-9">WelCome to my React Card-02</h1>
     <div className='grid grid-cols-3 gap-5'> 
-    <Card2></Card2>
-    <Card2></Card2>
-    <Card2></Card2>
+    <Card2 newTitle={newTitle} views={myObj.views} ></Card2>
+    <Card2 newTitle={title2} views={myObj.views2}></Card2>
+    <Card2 newTitle={lastTitle } views={myObj.views3}></Card2>
     </div>
 
     </div>
--- src/components/Card2/Card2.jsx
+++ src/components/Card2/Card2.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import CardImg from '/img/luahg-at-pain.jpg'
 
-const Card2 =()=>{
-    return(<>
+const Card2 =(props)=>{
+    console.log(props)
+    return(
+    <>
     <div className="card bg-base-100 shadow-xl">
     <figure><img src={CardImg} alt="Shoes" /></figure>
     <div className="card-body">
-        <h2 className="card-title">Shoes!</h2>
+        <h2 className="card-title ">{props.newTitle}</h2>
         <p>If a dog chews shoes whose shoes does he choose?</p>
+        <p className="font-medium">Views : {props.views} </p>
     </div>
     </div>
-    </>)
+    </>
+    )
 };
 export default Card2;
